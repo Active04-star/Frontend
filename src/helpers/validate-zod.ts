a0c153c1164feca4737081952645ed42df0b6268
Joshua-frDev <elvisteck046@outlook.com>
2024-12-09 19:57:09 -0500

    Validator zod & Schemas para formularios y respuestas de back

--- src/helpers/validate-zod.ts
+++ src/helpers/validate-zod.ts
@@ -1,5 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+
+/** Metodo para verificacion de errores de formulario, errores sujetos a constraints en los schemas
+ * En caso de contener un error, devuelve un objeto con un array de errores para cada campo
+ * 
+ * @param data Objeto para verificar
+ * @param schema zSchema (usualmente de registro, encontrados en la carpeta "types")
+ * @returns 
+ */
 export function zodValidate(data: unknown, schema: any) {
     const result = schema.safeParse(data);
 
--- src/types/zCenterRegisterSchema.ts
+++ src/types/zCenterRegisterSchema.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 
+const name_lenght_error = "El nombre debe ser de 3 - 50 caracteres";
+const address_lenght_error = "La direccion debe ser de 5 - 120 caracteres";
+
 /**Centro deportivo de formulario
  */
 export const CenterRegisterSchema = z.object({
-    name: z.string(),
+    name: z.string().min(3, name_lenght_error).max(50, name_lenght_error),
 
-    address: z.string(),
+    address: z.string().min(5, address_lenght_error).max(120, address_lenght_error),
 
-    manager: z.string().uuid(),
-
+    manager: z.string().uuid("No hay una id de manager valida en esta peticion"),
 });
