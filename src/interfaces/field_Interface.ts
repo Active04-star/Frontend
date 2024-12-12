cd2a834280dedff847b5bcee195f5e03688791dd
Active04-star <activeproject04@gmail.com>
2024-12-12 15:08:25 -0300

    modificacion field_interface

--- src/interfaces/field_Interface.ts
+++ src/interfaces/field_Interface.ts
@@ -1,3 +1,8 @@
+import { FieldStatus } from "@/enum/FieldStatus";
+import { IReservation } from "./reservation_Interface";
+import { ISportCategory } from "./sportCategory_interface";
+import { ISportCenter } from "./SportCenter_Interface";
+
 export interface IField {
   id: string;
   number: number;
