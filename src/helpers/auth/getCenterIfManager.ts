242e7a74689f9d151fd9c7441f4f7710e0912986
Fabrizio9898 <fabrizioandrade989@gmail.com>
2025-01-06 02:37:35 -0300

    update function

--- src/helpers/auth/getCenterIfManager.ts
+++ src/helpers/auth/getCenterIfManager.ts
@@ -1,14 +1,9 @@
 import { API_URL } from "@/config/config";
 import { fetchAndCatch } from "../errors/fetch-error-interceptor";
-import { UserRole } from "@/enum/userRole";
 import { IUser } from "@/types/zTypes";
 
 export async function getCenterIfManager(userData: IUser): Promise<void> {
     const {user,token}=userData
-   
-    
-    if (user.role === UserRole.MANAGER || user.role === UserRole.MAIN_MANAGER) {
-
         try {
             const data = await fetchAndCatch(`${API_URL}/manager/center/${user.id}`, {
                 method: "GET",
@@ -22,7 +17,4 @@ export async function getCenterIfManager(userData: IUser): Promise<void> {
         } catch (error) {
             throw error;
         }
-    } else {
-        return;
-    }
 }
--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -32,12 +32,10 @@ const LoginView: React.FC = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
-
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUserData({ ...userData, [name]: value });
   };
-
 
   useEffect(() => {
     const data = zodValidate(userData, UserLoginSchema);
@@ -49,12 +47,12 @@ const LoginView: React.FC = () => {
     }
   }, [userData]);
 
-
   const redirectToAuth = (email: string) => {
     // window.location.href = `${AUTH0_ISSUER_BASE_URL}/authorize?client_id=${AUTH0_CLIENT_ID}&response_type=code&scope=openid%20profile%20email&login_hint=${encodeURIComponent(email)}`
-    window.location.href = `api/auth/login?login_hint=${encodeURIComponent(email)}`
+    window.location.href = `api/auth/login?login_hint=${encodeURIComponent(
+      email
+    )}`;
   };
-
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -64,15 +62,16 @@ const LoginView: React.FC = () => {
 
     const validation = zodValidate(userData, UserLoginSchema);
 
-    if (validation.errors === undefined || validation.errors.email === undefined) {
+    if (
+      validation.errors === undefined ||
+      validation.errors.email === undefined
+    ) {
       const response = await getUserType(userData.email);
 
-      if(response.message === ApiStatusEnum.USER_IS_THIRD_PARTY) {
-
+      if (response.message === ApiStatusEnum.USER_IS_THIRD_PARTY) {
         redirectToAuth(userData.email.toLowerCase());
         return;
       }
-
     }
 
     if (Object.values(userData).find((data) => data === "") === "") {
@@ -85,7 +84,10 @@ const LoginView: React.FC = () => {
     // const data = zodValidate(userData, UserLoginSchema);
 
     if (!validation.success) {
-      swalCustomError("Error en Logueo", "Por favor corrige los errores antes de continuar.");
+      swalCustomError(
+        "Error en Logueo",
+        "Por favor corrige los errores antes de continuar."
+      );
 
       setIsSubmitting(false);
       return;
@@ -102,190 +104,179 @@ const LoginView: React.FC = () => {
       swalNotifySuccess("¡Bienvenido de nuevo!", "");
 
       setUserData(initialState);
-      getCenterIfManager(response);
+      if (user.role === UserRole.MAIN_MANAGER) {
+        getCenterIfManager(response);
+      }
 
       const roleRoutes = {
         [UserRole.USER]: "/user",
         [UserRole.MAIN_MANAGER]: "/manager",
         [UserRole.ADMIN]: "/admin",
-        [UserRole.MANAGER]:'/manager'
+        [UserRole.MANAGER]: "/manager",
       };
-      
+
       const route = roleRoutes[user.role];
       if (route) {
-         router.push(route);
-         return;
+        router.push(route);
+        return;
       }
 
       router.push("/");
     } catch (error) {
-
-      if (error instanceof ErrorHelper && error.message === ApiStatusEnum.USER_DELETED) {
+      if (
+        error instanceof ErrorHelper &&
+        error.message === ApiStatusEnum.USER_DELETED
+      ) {
         swalCustomError(ApiStatusEnum.USER_DELETED, "No se pudo logear");
-
       } else {
         AuthErrorHelper(error);
-
       }
 
       setIsSubmitting(false);
     }
-
   };
-
 
   return (
     <div className="bg-custom-dark min-h-screen flex flex-col items-center justify-center text-center">
-      {
-        isSubmitting ?
-          (
-            <>
-              <h1 className="text-4xl font-bold mb-8 font-serif text-white">
-                Cargando...
-              </h1>
-              <div className="w-32 h-32">
-                <LoadingCircle />
+      {isSubmitting ? (
+        <>
+          <h1 className="text-4xl font-bold mb-8 font-serif text-white">
+            Cargando...
+          </h1>
+          <div className="w-32 h-32">
+            <LoadingCircle />
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="text-5xl font-bold mb-8 font-serif text-white">
+            Active
+          </h1>
+          <form onSubmit={handleSubmit} className="w-full max-w-sm">
+            <div className="mb-6">
+              <label
+                className="block text-white mb-2 text-center font-medium text-lg"
+                htmlFor="username"
+              >
+                Usuario
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="email"
+                value={userData.email}
+                onChange={handleChange}
+                placeholder="Active123@mail.com"
+                className="w-full px-4 py-2 border-gray-300 rounded-lg bg-gray-200 focus:outline-none text-black font-sans"
+              />
+              {userData.email &&
+              errors !== null &&
+              errors.email !== undefined &&
+              errors?.email._errors !== undefined ? (
+                <span
+                  className="text-sm text-red-600"
+                  style={{ fontSize: "12px" }}
+                >
+                  {errors.email._errors}
+                </span>
+              ) : null}
+            </div>
+            <div className="mb-6 relative">
+              <label
+                className="block text-white mb-2 text-center font-medium text-lg"
+                htmlFor="password"
+              >
+                Contraseña
+              </label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={userData.password}
+                  onChange={handleChange}
+                  placeholder="******"
+                  className="w-full px-4 py-2 border-gray-300 rounded-lg bg-gray-200 focus:outline-none text-black font-sans"
+                />
+                <div
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <FaEyeSlash style={{ color: "black" }} />
+                  ) : (
+                    <FaEye style={{ color: "black" }} />
+                  )}
+                </div>
               </div>
-            </>
-          ) :
-          (
-            <>
-              <h1 className="text-5xl font-bold mb-8 font-serif text-white">
-                Active
-              </h1>
-              <form onSubmit={handleSubmit} className="w-full max-w-sm">
-                <div className="mb-6">
-                  <label
-                    className="block text-white mb-2 text-center font-medium text-lg"
-                    htmlFor="username"
+              {userData.password &&
+              errors !== null &&
+              errors.password !== undefined &&
+              errors?.password._errors !== undefined ? (
+                <>
+                  <span
+                    className="text-sm text-red-600"
+                    style={{ fontSize: "12px" }}
                   >
-                    Usuario
-                  </label>
-                  <input
-                    type="text"
-                    id="username"
-                    name="email"
-                    value={userData.email}
-                    onChange={handleChange}
-                    placeholder="Active123@mail.com"
-                    className="w-full px-4 py-2 border-gray-300 rounded-lg bg-gray-200 focus:outline-none text-black font-sans"
-                  />
-                  {userData.email &&
-                    errors !== null &&
-                    errors.email !== undefined &&
-                    errors?.email._errors !== undefined ? (
+                    {errors.password._errors[0]}
+                  </span>
+
+                  <div>
                     <span
                       className="text-sm text-red-600"
                       style={{ fontSize: "12px" }}
                     >
-                      {errors.email._errors}
+                      {errors.password._errors[1] !== undefined &&
+                      errors.password._errors[1].length > 0
+                        ? errors.password._errors[1]
+                        : null}
                     </span>
-                  ) : null}
-                </div>
-                <div className="mb-6 relative">
-                  <label
-                    className="block text-white mb-2 text-center font-medium text-lg"
-                    htmlFor="password"
-                  >
-                    Contraseña
-                  </label>
-                  <div className="relative">
-                    <input
-                      type={showPassword ? "text" : "password"}
-                      id="password"
-                      name="password"
-                      value={userData.password}
-                      onChange={handleChange}
-                      placeholder="******"
-                      className="w-full px-4 py-2 border-gray-300 rounded-lg bg-gray-200 focus:outline-none text-black font-sans"
-                    />
-                    <div
-                      className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? (
-                        <FaEyeSlash style={{ color: "black" }} />
-                      ) : (
-                        <FaEye style={{ color: "black" }} />
-                      )}
-                    </div>
                   </div>
-                  {userData.password &&
-                    errors !== null &&
-                    errors.password !== undefined &&
-                    errors?.password._errors !== undefined ? (
-                    <>
-                      <span
-                        className="text-sm text-red-600"
-                        style={{ fontSize: "12px" }}
-                      >
-                        {errors.password._errors[0]}
-                      </span>
+                </>
+              ) : null}
+            </div>
 
-                      <div>
-                        <span
-                          className="text-sm text-red-600"
-                          style={{ fontSize: "12px" }}
-                        >
-                          {errors.password._errors[1] !== undefined &&
-                            errors.password._errors[1].length > 0
-                            ? errors.password._errors[1]
-                            : null}
-                        </span>
-                      </div>
-                    </>
-                  ) : null}
-                </div>
+            <div className="w-auto flex justify-around">
+              <button
+                type="submit"
+                className="mt-5 bg-primary text-dark px-6 py-2 rounded hover:bg-yellow-700 bg-yellow-600"
+              >
+                Ingresar
+              </button>
+            </div>
 
+            <div className="mt-3">
+              <span className="mr-4">¿No tienes cuenta?</span>
+              <Link href={"/register"}>
+                <span className="text-yellow-600">Regístrate</span>
+              </Link>
+            </div>
 
-                <div className="w-auto flex justify-around">
+            <div className="flex justify-around py-5">
+              <div className="w-2/5 border-b border-white mb-3"></div>
+              <div className="mx-2">o</div>
+              <div className="w-2/5 border-b border-white mb-3"></div>
+            </div>
 
-                  <button
-                    type="submit"
-                    className="mt-5 bg-primary text-dark px-6 py-2 rounded hover:bg-yellow-700 bg-yellow-600"
-                  >
-                    Ingresar
-                  </button>
-                </div>
-
-                <div className="mt-3">
-                  <span className="mr-4">
-                    ¿No tienes cuenta?
-                  </span>
-                  <Link href={"/register"}>
-                    <span className="text-yellow-600">
-                      Regístrate
-                    </span>
-                  </Link>
-                </div>
-
-                <div className="flex justify-around py-5">
-                  <div className="w-2/5 border-b border-white mb-3"></div>
-                  <div className="mx-2">
-                    o
-                  </div>
-                  <div className="w-2/5 border-b border-white mb-3"></div>
-                </div>
-
-                <div className="w-auto flex justify-around">
-                  <Link
-                    type="submit"
-                    className="mt-5 bg-primary text-dark px-6 py-2 rounded bg-orange-100 text-black flex justify-between"
-                    href="api/auth/login">
-                    <Image
-                      src="https://auth.openai.com/assets/google-logo-NePEveMl.svg"
-                      alt="google icon"
-                      width={25}
-                      height={25}
-                      className="mr-2"
-                    />
-                    Continuar con Google
-                  </Link>
-                </div>
-              </form >
-            </>
-          )
-      }
+            <div className="w-auto flex justify-around">
+              <Link
+                type="submit"
+                className="mt-5 bg-primary text-dark px-6 py-2 rounded bg-orange-100 text-black flex justify-between"
+                href="api/auth/login"
+              >
+                <Image
+                  src="https://auth.openai.com/assets/google-logo-NePEveMl.svg"
+                  alt="google icon"
+                  width={25}
+                  height={25}
+                  className="mr-2"
+                />
+                Continuar con Google
+              </Link>
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 };
