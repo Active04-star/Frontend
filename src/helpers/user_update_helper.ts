05fe175092c40532303955e40debf28ed18d0154
Joshua-frDev <elvisteck046@outlook.com>
2025-01-07 23:18:00 -0500

    QuickFix

--- src/helpers/user_update_helper.ts
+++ src/helpers/user_update_helper.ts
@@ -2,10 +2,7 @@ import { API_URL } from "@/config/config";
 import { IPasswordUpdate, IUser, IUserUpdate } from "@/types/zTypes";
 import { fetchWithAuth } from "./errors/fetch-with-token-interceptor";
 
-export async function updateUser(
-  id: string,
-  body: Partial<IUserUpdate & IPasswordUpdate>
-): Promise<Omit<IUser, "token"> | null> {
+export async function updateUser(id: string, body: Partial<IUserUpdate & IPasswordUpdate>): Promise<Omit<IUser, "token"> | null> {
   let response = null;
 
   if (body.name !== undefined) {
@@ -16,33 +13,19 @@ export async function updateUser(
       },
       body: JSON.stringify({ name: body.name }),
     });
-  } else if (
-    body.password !== undefined &&
-    body.confirm_password !== undefined
-  ) {
-    response = await fetchWithAuth(`${API_URL}/NO_EXISTE_ESTA_RUTA_AUN/${id}`, {
+
+  }
+
+  if (body.password !== undefined && body.confirm_password !== undefined) {
+    response = await fetchWithAuth(`${API_URL}/auth/update-password/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        password: body.password,
-        confirm_password: body.confirm_password,
-      }),
+      body: JSON.stringify({ password: body.password, confirm_password: body.confirm_password }),
     });
   }
 
-    }
+  return { user: response };
 
-    if (body.password !== undefined && body.confirm_password !== undefined) {
-        response = await fetchWithAuth(`${API_URL}/auth/update-password/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ password: body.password, confirm_password: body.confirm_password }),
-        });
-    }
-
-    return { user: response };
 }
