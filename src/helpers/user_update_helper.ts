bd5484e6dfcd9d07b5c4605783885cc9570b7186
augusto <augustoaranda22@gmail.com>
2025-01-07 21:27:45 -0300

    sin cambios

--- src/helpers/user_update_helper.ts
+++ src/helpers/user_update_helper.ts
@@ -2,27 +2,35 @@ import { API_URL } from "@/config/config";
 import { IPasswordUpdate, IUser, IUserUpdate } from "@/types/zTypes";
 import { fetchWithAuth } from "./errors/fetch-with-token-interceptor";
 
-export async function updateUser(id: string, body: Partial<IUserUpdate & IPasswordUpdate>): Promise<Omit<IUser, "token"> | null> {
-    let response = null;
+export async function updateUser(
+  id: string,
+  body: Partial<IUserUpdate & IPasswordUpdate>
+): Promise<Omit<IUser, "token"> | null> {
+  let response = null;
 
-    if (body.name !== undefined) {
-        response = await fetchWithAuth(`${API_URL}/user/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ name: body.name })
-        });
+  if (body.name !== undefined) {
+    response = await fetchWithAuth(`${API_URL}/user/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: body.name }),
+    });
+  } else if (
+    body.password !== undefined &&
+    body.confirm_password !== undefined
+  ) {
+    response = await fetchWithAuth(`${API_URL}/NO_EXISTE_ESTA_RUTA_AUN/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password: body.password,
+        confirm_password: body.confirm_password,
+      }),
+    });
+  }
 
-    } else if (body.password !== undefined && body.confirm_password !== undefined) {
-        response = await fetchWithAuth(`${API_URL}/NO_EXISTE_ESTA_RUTA_AUN/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ password: body.password, confirm_password: body.confirm_password }),
-        });
-    }
-
-    return { user: response };
+  return { user: response };
 }
