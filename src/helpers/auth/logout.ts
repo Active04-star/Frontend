1dd3df565d6083faf512f27d9895bbf982c8f7f8
Leandro Escudero <leasoyt@gmail.com>
2025-01-10 15:00:45 -0300

    error bug logout

--- src/helpers/auth/logout.ts
+++ src/helpers/auth/logout.ts
@@ -1,12 +1,16 @@
 import { swalNotifySuccess } from "../swal/swal-notify-success";
 
 export function logout(fire_swal: boolean): void {
-    localStorage.removeItem("userSession");
-    localStorage.removeItem("sportCenter");
+  // Elimina la sesión del localStorage
+  localStorage.removeItem("userSession");
+  localStorage.removeItem("sportCenter");
 
-    if(fire_swal) {
-        swalNotifySuccess("¡Adiós!", "Tu sesión ha finalizado.");
-    }
+  // Muestra la notificación si es necesario
+  if (fire_swal) {
+    swalNotifySuccess("¡Adiós!", "Tu sesión ha finalizado.");
+  }
 
-    window.location.href = "/api/auth/logout";
+  // Construye la URL de logout basada en la variable de entorno
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "";
+  window.location.href = `${baseUrl}/api/auth/logout`;
 }
