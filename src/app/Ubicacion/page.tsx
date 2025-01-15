767b01b758e7e21137a7b9793e2f4e43c830d041
Fabrizio9898 <fabrizioandrade989@gmail.com>
2025-01-15 11:37:31 -0300

    ubicacion por reviews

---
+++ src/app/Ubicacion/page.tsx
@@ -0,0 +1,8 @@
+import UbicacionView from "@/views/Ubicacion/ubicacion.view";
+import React from "react";
+
+const UbicacionPage: React.FC = () => {
+  return <UbicacionView />;
+};
+
+export default UbicacionPage;
